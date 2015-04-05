8a1107e71ae88e90a3628dc72ade34f06d2ebcca
Joshua Snider <jsnider3@gmu.edu>
2015-04-05 18:03:59 -0400

    Chapter 5 bitwise IO

--- MoreOCamlBook/C45BitIO.ml
+++ MoreOCamlBook/C45BitIO.ml
@@ -106,25 +106,121 @@ let line_reader ch = {ch with
                             10 -> raise End_of_file
                             |x -> x);
   (*
-    The length field for this record is now invalid. 
+    Length field for this is invalid. 
   *)
   };;
 
 (*
   Page 26: Question 5
 *)
-
 type output =
   {
     output_char : char -> unit;
     out_channel_length : unit -> int;
   };;
 
+let output_of_channel ch = 
+  {
+    output_char = (fun c -> output_char ch c);
+    out_channel_length = (fun () -> int64_to_int (Out_channel.length ch));
+  };;
+
 let output_of_buffer bf = 
   {
     output_char = (fun c -> Buffer.add_char bf c);
-    out_channel_length = (fun c -> Buffer.length bf);
+    out_channel_length = (fun () -> Buffer.length bf);
   };;
+
+(* Start of chapter 5. *)
+
+type input_bits =
+  {
+    input : input;
+    mutable byte : int;
+    mutable bit : int;
+  };;
+
+let bool_to_int b = if b then 1 else 0;;
+
+(* My version of align. *)
+let loadbyte b =
+  b.byte <- int_of_char (b.input.input_char ());
+  b.bit <- 128;;
+
+let nextbit b = let ind = b.bit in
+  b.bit <- b.bit / 2;
+  b.byte land (ind) > 0;;
+
+let rec getbit b =
+  match b.bit with
+    0 -> loadbyte b; getbit b
+    |_ -> nextbit b;;
+
+(* Not tail recursive, but stack depth is bounded. 
+   Could be done with a fold? *)
+let rec getval b n = assert (n > 0 && n < 32);
+  match n with
+    1 -> bool_to_int (getbit b);
+    |_ -> (bool_to_int (getbit b)) + (getval b (n - 1) lsl 1);;
+
+(*
+  Page 34 Question 2
+*)
+let rec getval_32 b n = Int32.of_int_exn (getval b n);;
+
+type output_bits =
+  {
+    output : output;
+    mutable byte : int;
+    mutable bit : int;
+  };;
+
+let output_bits_of_output out =
+  {
+    output = out;
+    byte = 0;
+    bit = 7;
+  };;
+
+let flush o =
+  if o.bit < 7 then o.output.output_char (char_of_int o.byte);
+  o.byte <- 0;
+  o.bit <- 7;;
+
+let rec putbit o b = match o.bit with
+  -1 -> flush o; putbit o b;
+  |_ -> o.byte <- o.byte lor (b lsl o.bit);
+        o.bit <- o.bit - 1;; 
+
+(*
+  Page 34 Question 3
+*)
+let putbyte o v = o.byte <- v;
+  o.bit <- 0;
+  flush o;;
+
+let rec putval o v l = 
+  if o.bit = 7 && l = 8
+    then putbyte o v
+  else if l > 0 then
+    begin
+    let shift = 1 lsl (l - 1) in
+    putbit o (v land shift);
+    putval o (v land (lnot shift)) (l - 1)
+    end
+  ;; 
+
+(*
+  Page 34 Question 4
+*)
+let rec putval_32 o v = putval o (Int32.to_int_exn v) 32;;
+
+(*
+  Page 34 Question 5: Modifying a string in-place is deprecated,
+                      but could be done by removing the last
+                      char in a string whenever you change the byte
+                      and don't move to the next one.
+*)
 
 (*
 
@@ -139,17 +235,48 @@ let test_input_of_chars () =
   assert ("abc" = (input_string inp 4));;
 
 let test_input_of_channel () =
-  let inp = input_of_channel (open_in "C4IO.ml") in
+  let inp = input_of_channel (open_in "C45BitIO.ml") in
   assert ("open Core.Std;;" = (input_string inp 15));;
 
 let test_line_reader () =
-  let inp = line_reader(input_of_channel (open_in "C4IO.ml")) in
+  let inp = line_reader(input_of_channel (open_in "C45BitIO.ml")) in
   assert ("open Core.Std;;" = (input_string inp 95));;
+
+let test_getbit () = 
+  let bits = {input = input_of_chars ['a']; byte = 188; bit = 128} in
+  (* Reading 188 *)
+  assert (getbit bits = true);
+  assert (getbit bits = false);
+  assert (getbit bits = true);
+  assert (getbit bits = true);
+  assert (getbit bits = true);
+  assert (getbit bits = true);
+  assert (getbit bits = false);
+  assert (getbit bits = false);
+  (* Reading 'a' *)
+  assert (getbit bits = false);
+  assert (getbit bits = true);
+  assert (getbit bits = true);
+  assert (getbit bits = false);
+  assert (getbit bits = false);
+  assert (getbit bits = false);
+  assert (getbit bits = false);
+  assert (getbit bits = true);;
+
+let test_getval () =
+  let bits = {input = input_of_chars ['a']; byte = 188; bit = 128} in
+  assert ((getval bits 8) = 188);
+  assert ((getval bits 8) = 97);;
+
+let test_output () = ();;
 
 let main () = 
   test_input_of_chars ();
   test_input_of_channel ();
-  test_line_reader ();;
+  test_line_reader ();
+  test_getbit ();
+  test_getval ();
+  test_output ();;
 
 main ();;
 
